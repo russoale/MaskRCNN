cffa25f26321cc60b4d7b0cc916dea2c5892f96f
Alessandro Russo <alessandro.russo@allianz.de>
2018-12-01 20:41:34 +0100

    only use one GPU

--- samples/coco/coco.py
+++ samples/coco/coco.py
@@ -88,7 +88,8 @@ class CocoConfig(Config):
     IMAGES_PER_GPU = 2
 
     # Uncomment to train on 8 GPUs (default is 1)
-    GPU_COUNT = 2
+    # Parallel Model currently not working 
+    #GPU_COUNT = 2
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 1  # Person and background
