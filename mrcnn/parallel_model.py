feb485754dc94c539da73155c0461731a10e4d7d
Alessandro Russo <alessandro.russo@allianz.de>
2018-12-08 11:05:38 +0100

    update parallel model

--- mrcnn/parallel_model.py
+++ mrcnn/parallel_model.py
@@ -19,19 +19,19 @@ import keras.layers as KL
 import keras.models as KM
 
 
-def make_parallel(inner_model, gpu_count):
+def make_parallel(keras_model, gpu_count):
     """Creates a new wrapper model that consists of multiple replicas of
     the original model placed on different GPUs.
     """
     # Slice inputs. Slice inputs on the CPU to avoid sending a copy
     # of the full inputs to all GPUs. Saves on bandwidth and memory.
     input_slices = {name: tf.split(x, gpu_count)
-                    for name, x in zip(inner_model.input_names,
-                                       inner_model.inputs)}
+                    for name, x in zip(keras_model.input_names,
+                                       keras_model.inputs)}
 
-    output_names = inner_model.output_names
+    output_names = keras_model.output_names
     outputs_all = []
-    for i in range(len(inner_model.outputs)):
+    for i in range(len(keras_model.outputs)):
         outputs_all.append([])
 
     # Run the model call() on each GPU to place the ops there
@@ -39,14 +39,14 @@ def make_parallel(inner_model, gpu_count):
         with tf.device('/gpu:%d' % i):
             with tf.name_scope('tower_%d' % i):
                 # Run a slice of inputs through this replica
-                zipped_inputs = zip(inner_model.input_names,
-                                    inner_model.inputs)
+                zipped_inputs = zip(keras_model.input_names,
+                                    keras_model.inputs)
                 inputs = [
                     KL.Lambda(lambda s: input_slices[name][i],
                               output_shape=lambda s: (None,) + s[1:])(tensor)
                     for name, tensor in zipped_inputs]
                 # Create the model replica and get the outputs
-                outputs = inner_model(inputs)
+                outputs = keras_model(inputs)
                 if not isinstance(outputs, list):
                     outputs = [outputs]
                 # Save the outputs for merging back together later
@@ -85,9 +85,10 @@ class ParallelModel(KM.Model):
         keras_model: The Keras model to parallelize
         gpu_count: Number of GPUs. Must be > 1
         """
-        outputs = make_parallel(keras_model, gpu_count)
+        merged_outputs = make_parallel(
+            keras_model=keras_model, gpu_count=gpu_count)
         super(ParallelModel, self).__init__(inputs=keras_model.inputs,
-                                            outputs=outputs)
+                                            outputs=merged_outputs)
         self.inner_model = keras_model
 
     def __getattribute__(self, attrname):
